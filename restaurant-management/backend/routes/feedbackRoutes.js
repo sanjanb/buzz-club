f524aa2bf4fd693322371202b78cd61743567106
sanjan B M <acharyasanjanbm@gmail.com>
2024-07-10 18:36:25 +0530

    feedbackRoutes  update

--- restaurant-management/backend/routes/feedbackRoutes.js
+++ restaurant-management/backend/routes/feedbackRoutes.js
@@ -1,14 +1,17 @@
-// routes/feedbackRoutes.js
 const express = require("express");
 const {
   getFeedback,
+  getFeedbackById,
   addFeedback,
+  updateFeedback,
+  deleteFeedback,
 } = require("../controllers/feedbackController");
 const router = express.Router();
 
 router.get("/", getFeedback);
+router.get("/:id", getFeedbackById);
 router.post("/", addFeedback);
-
-// Add other CRUD operations as needed
+router.put("/:id", updateFeedback);
+router.delete("/:id", deleteFeedback);
 
 module.exports = router;
